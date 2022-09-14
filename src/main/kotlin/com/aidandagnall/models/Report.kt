fc2d5be22ee7a210fe0f540379df9e1ba60a4c9e
Aidan Dagnall <aidandagnall@gmail.com>
2022-09-14 02:03:21 +0100

    Add email to reports

--- src/main/kotlin/com/aidandagnall/models/Report.kt
+++ src/main/kotlin/com/aidandagnall/models/Report.kt
@@ -4,6 +4,6 @@ import org.litote.kmongo.Id
 import java.time.Instant
 import java.util.*
 
-data class Report(val time: Instant, val room: Id<Room>, val popularity: Popularity?, val removalChance: RemovalChance?)
+data class Report(val time: Instant, val room: Id<Room>, val popularity: Popularity?, val removalChance: RemovalChance?, val email: String?)
 
 data class ReportDTO(val room: String, val popularity: Popularity?, val removalChance: RemovalChance?)
--- src/main/kotlin/com/aidandagnall/routes/ReportRoutes.kt
+++ src/main/kotlin/com/aidandagnall/routes/ReportRoutes.kt
@@ -4,12 +4,17 @@ import com.aidandagnall.Constants
 import com.aidandagnall.models.Report
 import com.aidandagnall.models.ReportDTO
 import com.aidandagnall.models.Room
+import com.aidandagnall.models.UserSession
+import com.aidandagnall.tokenAuthenticationCredentials
+import com.google.common.hash.Hashing
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.litote.kmongo.*
+import java.nio.charset.StandardCharsets
 import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -18,23 +23,28 @@ fun Route.reportRouting() {
     val db = KMongo.createClient(Constants.CONNECTION_STRING).getDatabase("labs")
     val roomCollection = db.getCollection<Room>()
     val reportCollection = db.getCollection<Report>()
+    val sessions = db.getCollection<UserSession>("user_session")
     route("/report") {
-        options {
+        authenticate {
 
-        }
-        post {
-            val reportInfo: ReportDTO = call.receive()
-            val room = roomCollection.findOne(Room::name eq reportInfo.room)
-            if (room == null) {
-                call.respond(HttpStatusCode.BadRequest)
+            post {
+                val tokenHash = call.request.authorization()
+                    ?.let { token -> Hashing.sha256().hashString(token, StandardCharsets.UTF_8).toString() }
+                val reportInfo: ReportDTO = call.receive()
+                val room = roomCollection.findOne(Room::name eq reportInfo.room)
+                if (room == null) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+
+                if (reportInfo.popularity == null && reportInfo.removalChance == null) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+                val session =
+                    sessions.findOne { UserSession::tokenHash eq tokenHash } ?: return@post
+                reportCollection.insertOne(Report(ZonedDateTime.now(ZoneId.of("Europe/London")).toInstant(), room!!._id, reportInfo.popularity, reportInfo.removalChance, email = session.email))
+
+                call.respond(HttpStatusCode.Created)
             }
-
-            if (reportInfo.popularity == null && reportInfo.removalChance == null) {
-                call.respond(HttpStatusCode.BadRequest)
-            }
-            reportCollection.insertOne(Report(ZonedDateTime.now(ZoneId.of("Europe/London")).toInstant(), room!!._id, reportInfo.popularity, reportInfo.removalChance))
-
-            call.respond(HttpStatusCode.Created)
         }
     }
 }
