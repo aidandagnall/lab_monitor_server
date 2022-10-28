9b225bd156a47c2cc6163aa9778a9e45e5e44bcd
Aidan Dagnall <aidandagnall@gmail.com>
2022-10-28 23:21:06 +0100

    Use audience value for host

--- src/main/kotlin/com/aidandagnall/Application.kt
+++ src/main/kotlin/com/aidandagnall/Application.kt
@@ -2,8 +2,7 @@ package com.aidandagnall
 
 import com.aidandagnall.dao.DatabaseFactory
 import com.aidandagnall.plugins.*
-//import com.auth0.jwk.JwkProvider
-//import com.auth0.jwk.JwkProviderBuilder
+import io.github.cdimascio.dotenv.dotenv
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
@@ -12,7 +11,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     install(CORS) {
-        allowHost("lab-monitor.herokuapp.com", schemes = listOf("http", "https"))
+        allowHost(dotenv()["JWT_AUDIENCE"], schemes = listOf("http", "https"))
         allowHeader(HttpHeaders.AccessControlAllowOrigin)
         allowHeader(HttpHeaders.ContentType)
         allowMethod(HttpMethod.Options)
