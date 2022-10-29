15790cef21a57723f469136c367740614d287f44
Aidan Dagnall <aidandagnall@gmail.com>
2022-10-29 15:58:19 +0100

    Add option in .env to allow web connections

--- src/main/kotlin/com/aidandagnall/Application.kt
+++ src/main/kotlin/com/aidandagnall/Application.kt
@@ -2,6 +2,7 @@ package com.aidandagnall
 
 import com.aidandagnall.dao.DatabaseFactory
 import com.aidandagnall.plugins.*
+import io.github.cdimascio.dotenv.Dotenv
 import io.github.cdimascio.dotenv.dotenv
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -10,17 +11,19 @@ import io.ktor.server.plugins.cors.routing.*
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-    install(CORS) {
-        allowHost(dotenv()["JWT_AUDIENCE"], schemes = listOf("http", "https"))
-        allowHeader(HttpHeaders.AccessControlAllowOrigin)
-        allowHeader(HttpHeaders.ContentType)
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Post)
-        allowMethod(HttpMethod.Get)
-        allowHeader("authorization")
-        anyHost()
-        allowCredentials = true
-        allowNonSimpleContentTypes = true
+    if (dotenv()["ALLOW_WEB"] == "true") {
+        install(CORS) {
+            allowHost(dotenv()["JWT_AUDIENCE"], schemes = listOf("http", "https"))
+            allowHeader(HttpHeaders.AccessControlAllowOrigin)
+            allowHeader(HttpHeaders.ContentType)
+            allowMethod(HttpMethod.Options)
+            allowMethod(HttpMethod.Post)
+            allowMethod(HttpMethod.Get)
+            allowHeader("authorization")
+            anyHost()
+            allowCredentials = true
+            allowNonSimpleContentTypes = true
+        }
     }
     DatabaseFactory.init()
     configureRouting()
